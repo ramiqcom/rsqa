d9493a4ecfa60098b27225b885db6f9d44712942
Ramadhan <ramiqcom@gmail.com>
2023-01-22 12:01:16 +0700

    again

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import '../styles/globals.css';
-import 'react-range-slider-input/dist/style.css';
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
