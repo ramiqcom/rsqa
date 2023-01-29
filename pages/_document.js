7c48a47a742e9581c965a01c0abf551529dedf82
Ramadhan <ramiqcom@gmail.com>
2023-01-29 23:23:47 +0700

    add frame

---
+++ pages/_document.js
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+      </Head>
+      <body style={{ margin: '0' }}>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
